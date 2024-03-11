554cf02a5c9ad692b321583250863baa7187e261
shawkath646 <shawkath646@gmail.com>
2024-03-11 13:21:09 +0600

    Fixed: dependency problems

--- src/components/GoodBye.tsx
+++ src/components/GoodBye.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion"
-import { getBrandData } from "../../../NPM Library/shas-app-controller/src/client";
+import { getBrandData } from "shas-app-controller/client";
 
 const GoodBye: React.FC<any> = () => {
 
